5254bcd34a1080ab8f9f5320b3ca192bcba37703
abdulrazig Mohammed <abduadam688@gmail.com>
2025-09-09 20:36:05 -0400

    ""create an android app to let user enter thier names and say hello the users"

--- app/src/main/java/edu/temple/classactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/classactivity/MainActivity.kt
@@ -1,14 +1,29 @@
 package edu.temple.classactivity
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // val myButton = findViewById<button>(R.id.clickMeButton)
+        val textTextView = findViewById<TextView>(R.id.messageTextView)
+        val editText = findViewById<EditText>(R.id.editTextText)
+
+        findViewById<Button>(R.id.clickMeButton).setOnClickListener {
+            val name = editText.text.toString()
+
+            if (name.isNotEmpty()) {
+                textTextView.text = "Hello $name! Welcome"
+                editText.setText("")
+            } else {
+                textTextView.text = "Please enter your name"
+            }
+        }
     }
 }
